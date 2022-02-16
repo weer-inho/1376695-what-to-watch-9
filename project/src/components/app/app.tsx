582b49f82495ebf79962e43e1f1fd155ef7b0b1c
Руслан <60976203+weer-inho@users.noreply.github.com>
2022-02-16 22:43:48 +0300

    добавил статичные компоненты

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,8 +1,19 @@
-import FilmCard from '../film-card/film-card';
+// import FilmCard from '../film-card/film-card';
+// import SignIn from '../signin/sign-in';
+// import Player from '../player/player';
+// import Mylist from '../mylist/mylist';
+import MainPage from '../main-page/main-page';
 
-function App():JSX.Element {
+type AppProps = {
+  title: string,
+  janre: string,
+  year: number,
+  numberOfCards: number
+}
+
+function App({title, janre, year, numberOfCards}: AppProps):JSX.Element {
   return (
-    <FilmCard/>
+    <MainPage title={title} janre={janre} year={year} numberOfCards={numberOfCards}/>
   );
 }
 
--- project/src/components/film-card/film-card.tsx
+++ project/src/components/film-card/film-card.tsx
@@ -1,64 +1,13 @@
 function FilmCard():JSX.Element {
   return (
-    <section className="film-card">
-      <div className="film-card__bg">
-        <img src="img/bg-the-grand-budapest-hotel.jpg" alt="The Grand Budapest Hotel"/>
+    <article className="small-film-card catalog__films-card">
+      <div className="small-film-card__image">
+        <img src="img/fantastic-beasts-the-crimes-of-grindelwald.jpg" alt="Fantastic Beasts: The Crimes of Grindelwald" width="280" height="175"/>
       </div>
-
-      <h1 className="visually-hidden">WTW</h1>
-
-      <header className="page-header film-card__head">
-        <div className="logo">
-          <a className="logo__link">
-            <span className="logo__letter logo__letter--1">W</span>
-            <span className="logo__letter logo__letter--2">T</span>
-            <span className="logo__letter logo__letter--3">W</span>
-          </a>
-        </div>
-
-        <ul className="user-block">
-          <li className="user-block__item">
-            <div className="user-block__avatar">
-              <img src="img/avatar.jpg" alt="User avatar" width="63" height="63"/>
-            </div>
-          </li>
-          <li className="user-block__item">
-            <a className="user-block__link">Sign out</a>
-          </li>
-        </ul>
-      </header>
-
-      <div className="film-card__wrap">
-        <div className="film-card__info">
-          <div className="film-card__poster">
-            <img src="img/the-grand-budapest-hotel-poster.jpg" alt="The Grand Budapest Hotel poster" width="218" height="327"/>
-          </div>
-
-          <div className="film-card__desc">
-            <h2 className="film-card__title">The Grand Budapest Hotel</h2>
-            <p className="film-card__meta">
-              <span className="film-card__genre">Drama</span>
-              <span className="film-card__year">2014</span>
-            </p>
-
-            <div className="film-card__buttons">
-              <button className="btn btn--play film-card__button" type="button">
-                <svg viewBox="0 0 19 19" width="19" height="19">
-                  <use href="#play-s"></use>
-                </svg>
-                <span>Play</span>
-              </button>
-              <button className="btn btn--list film-card__button" type="button">
-                <svg viewBox="0 0 19 20" width="19" height="20">
-                  <use href="#add"></use>
-                </svg>
-                <span>My list</span>
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+      <h3 className="small-film-card__title">
+        <a className="small-film-card__link" href="film-page.html">Fantastic Beasts: The Crimes of Grindelwald</a>
+      </h3>
+    </article>
   );
 }
 
