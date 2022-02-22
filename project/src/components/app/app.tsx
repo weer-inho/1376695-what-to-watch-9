56a0b4c8cf294af6e779832237deb8ac096e9613
Руслан <60976203+weer-inho@users.noreply.github.com>
2022-02-22 15:52:49 +0300

    фикс компонента с пропсами

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -5,7 +5,7 @@ import SignIn from '../signin/sign-in';
 import Player from '../player/player';
 import Mylist from '../mylist/mylist';
 import AddReview from '../add-review/add-review';
-// import MainPage from '../main-page/main-page';
+import MainPage from '../main-page/main-page';
 
 type AppProps = {
   title: string,
@@ -19,12 +19,13 @@ function App({title, janre, year, numberOfCards}: AppProps):JSX.Element {
     //
     <BrowserRouter>
       <Routes>
-        {/*<Route*/}
-        {/*  path={AppRoute.Main}*/}
-        {/*  element={*/}
-        {/*    <MainPage title={title} janre={janre} year={year} numberOfCards={numberOfCards}/>*/}
-        {/*  }>*/}
-        {/*</Route>*/}
+        <Route
+          path={AppRoute.Main}
+          element={
+            <MainPage title={title} janre={janre} year={year} numberOfCards={numberOfCards}/>
+          }
+        >
+        </Route>
         <Route path={AppRoute.SignIn} element={<SignIn/>}></Route>
         <Route path={AppRoute.MyList} element={<Mylist/>}></Route>
         <Route path={AppRoute.Film} element={<FilmCard/>}></Route>
