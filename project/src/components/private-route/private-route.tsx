41d5f1d8b3297ac3f7c462c208f602960011f18c
Руслан <60976203+weer-inho@users.noreply.github.com>
2022-02-22 16:00:46 +0300

    добавил private-route

---
+++ project/src/components/private-route/private-route.tsx
@@ -0,0 +1,18 @@
+import {AuthorizationStatus} from '../../const';
+
+type PrivateRouteProps = {
+  authorizationStatus: AuthorizationStatus;
+  children: JSX.Element;
+}
+
+function PrivateRoute(props: PrivateRouteProps):JSX.Element {
+  const {authorizationStatus, children} = props;
+
+  return (
+    authorizationStatus === AuthorizationStatus.Auth
+    ? children
+    : <Navigate to={AppRoute.SignIn}/>
+  );
+}
+
+export default PrivateRoute;
